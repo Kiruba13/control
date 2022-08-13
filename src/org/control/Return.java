82f94067ae8bfef99bb6d9acb744befedbc2ddad
kiruba13 <kiruba94ashok@gmial.com>
2022-08-13 13:55:40 +0530

    sample

---
+++ src/org/control/Return.java
@@ -0,0 +1,21 @@
+package org.control;
+
+public class Return {
+	private int KK() {
+		// TODO Auto-generated method stub
+
+	int a =10,b=25;
+	int c = a+b;
+			return c;
+	
+	}
+	
+	
+	public static void main(String[] args) {
+		Return r = new Return();
+		int kk = r.KK();
+		System.out.println(kk);
+	}
+	
+
+}
---
+++ src/org/control/Switch1.java
@@ -0,0 +1,24 @@
+package org.control;
+
+public class Switch1 {
+	public static void main(String[] args) {
+		
+		String s= "sat" ;
+		switch (s) {
+		case ("sat"):
+			System.out.println("Saturday");
+		break;
+		
+		case ("sun"):
+			System.out.println("Sunday");
+		break;
+		
+		default:
+		System.out.println("Weekdays");
+		
+		}
+		
+		
+	}
+
+}
